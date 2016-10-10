56fc0651d5faa103d2ac44a4ad1bb6d0304d77f5
Edipo <edipox@gmail.com>
2016-10-10 16:52:00 -0300

    Elementary icons added

--- main.js
+++ main.js
@@ -10,16 +10,14 @@ app.on('window-all-closed', function() {
 let quitApp = function(){ app.quit() }
 
 app.on('ready', function() {
-  var size = { width: 800, height: 592 };
+  var size = { width: 400, height: 500 };
   mainWindow = new BrowserWindow({
     trasnparent: true,
     width: size.width,
     height: size.height,
     frame: false,
-    minWidth: 400,
-    minHeight: 500,
-    /* maxWidth: size.width,
-    maxHeight: size.height,*/
+    minWidth: size.width,
+    minHeight: size.height,
     radii: [10,10,10,10]
   });
 
