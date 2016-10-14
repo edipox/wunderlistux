4b6b86e839162f8abb7285bb27bd0aa77b9c3128
Edipo <edipox@gmail.com>
2016-10-14 13:50:29 -0300

    First release issues fixed

---
+++ releases/Wunderlistux-linux-x64/resources/app/js/controls.js
@@ -0,0 +1,57 @@
+jq(document).on("click", "#close-btn", function(e) {
+    remote.getCurrentWindow().close();
+});
+
+jq(document).on("click", "#min-btn", function(e) {
+    remote.getCurrentWindow().minimize();
+});
+var maximize = function(e) {
+    var window = remote.getCurrentWindow();
+    if (!window.isMaximized()) {
+        window.maximize();
+    } else {
+        window.unmaximize();
+    }
+};
+jq(document).on("dblclick", "#controls", maximize);
+jq(document).on("click", "#max-btn", maximize);
+
+jq(document).on("click", "#settings-btn", function(e) {
+    jq("#settings-panel").appendTo("#popup");
+    jq("#popup").show();
+    jq("#theme-selector").trigger('click').attr("size","2");
+
+});
+jq(document).on("mouseup", ":not(#settings-btn), :not(#settings-panel)", function() {
+    jq("#popup").hide();
+});
+
+jq(document).on('keydown', function(e) {
+    if (e.which == 27) jq("#popup").hide();
+});
+
+jq(document).on("click", "#bell", function(){
+  getWebview().send('click-bell');
+});
+jq(document).on("click", "#conv", function(){
+  getWebview().send("click-conversations")
+});
+
+jq(document).on("click", "#sort", function(e, v){
+  getWebview().send("click-sort", v)
+});
+
+jq(document).on("click", "#more", function(e, v){
+  getWebview().send("click-more", v)
+});
+
+jq(document).on("click", "#share", function(e, v){
+  getWebview().send("click-share", v)
+});
+
+
+jq(document).on("change", "#theme-selector", function(e) {
+    var theme = jq(e.target).find("option:selected").text();
+    setCustomTheme(theme)
+    jq("#popup").hide();
+});
