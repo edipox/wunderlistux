fe47143c25f39c8d85d1fe497fdb8e3807c985e2
Edipo <edipox@gmail.com>
2016-10-26 21:22:59 -0300

    Add release related files
    Former-commit-id: fb2c3b3665273199d9de86721ef9a0bee6237a2f

--- releases/Wunderlistux-linux-ia32/resources/app/bin/build-linux-ia32.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-APPNAME=Wunderlistux
-VERSION=1.2.7
-electron-packager . $APPNAME --platform=linux --arch=ia32 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=../images/wunderlist.png
