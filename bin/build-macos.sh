a9ba1bd6014194d6bc91a67def7c76ceff1fe7cb
Edipo Vinicius da Silva <edipox@gmail.com>
2017-03-03 12:12:08 -0300

    Fix #2 drag to-dos reorder
    Former-commit-id: 6b10000980c54c9c3687df06ee5d64bcfdb7d77a
    Former-commit-id: b7bc761a634234dac2ea167480e8a38fa0da9cab

--- bin/build-macos.sh
+++ bin/build-macos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Wunderlistux
-VERSION=1.2.7
+VERSION=1.6.1
 electron-packager . $APPNAME --version=$VERSION --platform=darwin --arch=x64 --icon=images/icon.icns  --appname=$APPNAME --out=releases --overwrite=true --asar=false
