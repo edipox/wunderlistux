8224074044be9b58772c25d6e83e7906ae18745c
Robert Lu <robberphex@gmail.com>
2017-04-10 01:00:35 +0800

    avoid global package, improve doc
    Former-commit-id: c063570d5216274f2abe08dbfbcf78147206255b
    Former-commit-id: e9bfa0a79529d358c17dae253ef7e1430203bdb7

--- bin/build-linux-ia32.sh
+++ bin/build-linux-ia32.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd $(dirname $0)/../
+
 APPNAME=Wunderlistux
 VERSION=1.6.1
-electron-packager . $APPNAME --platform=linux --arch=ia32 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=dist --overwrite=true --icon=../images/wunderlist.png
+./node_modules/.bin/electron-packager . $APPNAME \
+  --platform=linux --arch=ia32 --version=$VERSION \
+  --overwrite=true --asar=false --app_version=0.0.1 \
+  --appname=$APPNAME --out=dist --overwrite=true \
+  --icon=../images/wunderlist.png
