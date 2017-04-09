c063570d5216274f2abe08dbfbcf78147206255b
Robert Lu <robberphex@gmail.com>
2017-04-10 01:00:35 +0800

    avoid global package, improve doc

--- bin/build-linux-x64.sh
+++ bin/build-linux-x64.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd $(dirname $0)/../
+
 APPNAME=Wunderlistux
 VERSION=1.6.1
-electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=dist --overwrite=true --icon=../images/wunderlist.png
+./node_modules/.bin/electron-packager . $APPNAME \
+  --platform=linux --arch=x64 --electron-version=$VERSION \
+  --overwrite=true --asar=false --app_version=0.0.1 \
+  --appname=$APPNAME --out=dist --overwrite=true \
+  --icon=../images/wunderlist.png
