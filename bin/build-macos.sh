9fdbbf1dc1f100edb44588c31a9205fcec603209
Robert Lu <robberphex@gmail.com>
2017-04-10 00:45:50 +0800

    move to dist dir

--- bin/build-macos.sh
+++ bin/build-macos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Wunderlistux
 VERSION=1.6.1
-electron-packager . $APPNAME --version=$VERSION --platform=darwin --arch=x64 --icon=images/icon.icns  --appname=$APPNAME --out=releases --overwrite=true --asar=false
+electron-packager . $APPNAME --version=$VERSION --platform=darwin --arch=x64 --icon=images/icon.icns  --appname=$APPNAME --out=dist --overwrite=true --asar=false
