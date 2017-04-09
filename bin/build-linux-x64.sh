d08816fee0769e10a85931997a3b6fff44be9dcb
Robert Lu <robberphex@gmail.com>
2017-04-10 00:45:50 +0800

    move to dist dir
    Former-commit-id: 9fdbbf1dc1f100edb44588c31a9205fcec603209
    Former-commit-id: e80ba2d62eff9698531a400dc747257194efcf3d

--- bin/build-linux-x64.sh
+++ bin/build-linux-x64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Wunderlistux
 VERSION=1.6.1
-electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=../images/wunderlist.png
+electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=dist --overwrite=true --icon=../images/wunderlist.png
